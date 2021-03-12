7bce31579f354beca75833b8ab1197a1acae9a81
yodo1DSTeam <76093078+yodo1DSTeam@users.noreply.github.com>
2021-03-11 20:33:40 -0600

    Update CCPA.cs

--- Scripts/CCPA.cs
+++ Scripts/CCPA.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Yodo1.MAS;
 
 public class CCPA : MonoBehaviour
 {
@@ -51,12 +52,12 @@ public class CCPA : MonoBehaviour
        
     //Save user preference for YES 
 		PlayerPrefs.SetInt("CCPA", 1);
-        Yodo1U3dAds.SetDoNotSell(true);
+        Yodo1U3dMas.SetCCPA(true);
         PlayerPrefs.Save();
         Debug.Log("User Agree with CCPA");
         Text ConsoleText = GetComponent<AdsManager>().ConsoleText;
         ConsoleText.text = "User Agree with CCPA";
-        Yodo1U3dAds.InitializeSdk();
+        Yodo1U3dMas.InitializeSdk();
 
         if (CCPAPopup.activeSelf)
             CCPAPopup.SetActive(false);
@@ -69,12 +70,12 @@ public class CCPA : MonoBehaviour
     //Save user preference for NO,
 	//and initialize the SDK with this privacy parameters
         PlayerPrefs.SetInt("CCPA", 0);
-        Yodo1U3dAds.SetDoNotSell(false);
+        Yodo1U3dMas.SetCCPA(false); ;
         PlayerPrefs.Save();
         Debug.Log("User Not Agree with CCPA");
         Text ConsoleText = GetComponent<AdsManager>().ConsoleText;
         ConsoleText.text = "User Not Agree with CCPA";
-        Yodo1U3dAds.InitializeSdk();
+        Yodo1U3dMas.InitializeSdk();
 
         if (CCPAPopup.activeSelf)
             CCPAPopup.SetActive(false);
